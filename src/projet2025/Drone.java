fb1f01ccd3cb74fea81f6d6335a63c74bffe1c7f
haruka <harukamiura0702@gmail.com>
2025-12-18 13:31:32 +0100

    first commit

---
+++ src/projet2025/Drone.java
@@ -0,0 +1,13 @@
+package projet2025;
+
+public class Drone {
+	
+	int id;         //droneID
+    int x, y;       //positionnement
+
+	public Drone(int id) {
+		this.id = id;
+		this.x = 0; this.y = 0;
+	}
+
+}
